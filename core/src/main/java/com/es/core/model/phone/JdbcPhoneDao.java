1131af1f1214a128791971d6f8c322b8bec201a5
trowqe <trowqe@gmail.com>
2019-08-08 03:43:34 +0300

    task 2.1 - com.es.core.model.phone.JdbcPhoneDao implementation task 2.2 - BD configuration core/src/main/resources/conf/application.properties

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -2,19 +2,50 @@ package com.es.core.model.phone;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Optional;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
-public class JdbcPhoneDao implements PhoneDao{
+public class JdbcPhoneDao implements PhoneDao {
     @Resource
     private JdbcTemplate jdbcTemplate;
 
     public Optional<Phone> get(final Long key) {
-        throw new UnsupportedOperationException("TODO");
+        Phone phone = jdbcTemplate.queryForObject("select * from phones where id = ?", new Object[]{key}, new PhoneRowMappper());
+
+        phone.setColors(getColorSet(key));  // говно какое-то
+
+        return Optional.ofNullable(phone);
+    }
+
+    private List<Long> getPhoneColorsIdList(Long phoneId) {
+        return jdbcTemplate.query("select colorId from phone2color where phoneId=?",
+                (resultSet, i) -> resultSet.getLong("colorId"), phoneId);
+    }
+
+    private Color getColorCodeById(Long colorId) {
+        return jdbcTemplate.queryForObject(
+                "select code from colors where id = ?",
+                new Object[]{colorId}, (resultSet, i) -> {
+                    Color color = new Color();
+                    color.setId(colorId);
+                    color.setCode(resultSet.getString("code"));
+                    return color;
+                });
+    }
+
+    private Set<Color> getColorSet(Long phoneId) {
+        List<Long> phoneColorsIdList = getPhoneColorsIdList(phoneId);
+
+        return phoneColorsIdList.stream()
+                .map(this::getColorCodeById)
+                .collect(Collectors.toSet());
     }
 
     public void save(final Phone phone) {
@@ -22,6 +53,10 @@ public class JdbcPhoneDao implements PhoneDao{
     }
 
     public List<Phone> findAll(int offset, int limit) {
-        return jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+        //return jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+        List<Phone> phones = jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+        phones.forEach(p -> p.setColors(getColorSet(p.getId())));
+        return phones;
     }
 }
+
--- web/src/main/webapp/WEB-INF/pages/productList.jsp
+++ web/src/main/webapp/WEB-INF/pages/productList.jsp
@@ -2,28 +2,39 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <p>
-  Hello from product list!
+    Hello from product list!
 </p>
 <p>
-  Found <c:out value="${phones.size()}"/> phones.
-  <table border="1px">
+    Found
+    <c:out value="${phones.size()}"/> phones.
+<table border="1px">
     <thead>
-      <tr>
+    <tr>
         <td>Image</td>
         <td>Brand</td>
         <td>Model</td>
         <td>Price</td>
-      </tr>
+    </tr>
     </thead>
     <c:forEach var="phone" items="${phones}">
-      <tr>
-        <td>
-          <img src="https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/${phone.imageUrl}">
-        </td>
-        <td>${phone.brand}</td>
-        <td>${phone.model}</td>
-        <td>$ ${phone.price}</td>
-      </tr>
+        <tr>
+            <td>
+                <img src="https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/${phone.imageUrl}">
+            </td>
+            <td>${phone.brand}</td>
+            <td>${phone.model}</td>
+            <td>${phone.price}</td>
+
+            <td>
+                <c:forEach var="color" items="${phone.colors}">
+                    <li>
+                        <input type="radio" name="colorCode" value="${color.code}"/>
+                            ${color.code}<br><br>
+                    </li>
+
+                </c:forEach>
+            </td>
+
+        </tr>
     </c:forEach>
-  </table>
-</p>
+</table>
